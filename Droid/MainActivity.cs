10bdddb51ab64d57319a1cc02bab8a3b4fe18a4f
onion <onion.tsai@jetfusion.com.tw>
2016-09-10 17:48:12 +0800

    add textview

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -19,8 +19,12 @@ namespace TestJenkins.Droid
 			// Get our button from the layout resource,
 			// and attach an event to it
 			Button button = FindViewById<Button>(Resource.Id.myButton);
-
-			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+			TextView text1 = FindViewById<TextView>(Resource.Id.textView1);
+			button.Click += delegate { 
+				button.Text = string.Format("{0} clicks!", count);
+				text1.Text = count.ToString();
+				count++;
+			};
 		}
 	}
 }
